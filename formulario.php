03b8251733347b86be47aa9f0f97fe25ee37ee1e
YeroverAli <yeroveraligonzalez@alumno.ieselrincon.es>
2025-10-01 12:20:47 +0100

    Segunda subida

---
+++ formulario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <label for="email">Correo electrónico:</label>
+    <input id="email" type="email">
+    <p>Elija su módulo</p>
+    <select id="modulo">
+        <option value="dew">dew</option>
+        <option value="dor">dor</option>
+        <option value="dpl">dpl</option>
+        <option value="dsw">dsw</option>
+        <option value="soj">soj</option>
+        <option value="cl4">cl4</option>
+        <option value="e1b">e1b</option>
+        <option value="ipw">ipw</option>
+    </select>
+    <br><br>
+    <p>Asunto</p>
+    <input type="text" ></input>
+    <label for="asunto">
+</head>
+<body>
+    
+</body>
+</html>
